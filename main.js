2605355366943b936e3275a9a6babf83d2929ecb
JeanLarez <jeanlarez@gmail.com>
2022-10-24 22:34:57 -0300

    Integración de menu-desktop y menu-mobile mas funcionalidad de icono de hamburguesa

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 let menuEmail = document.querySelector(".navbar-email")
 let desktopMenu = document.querySelector(".desktop-menu")
+let menuHamIcon = document.querySelector(".menu")
+let mobileMenu = document.querySelector(".mobile-menu")
 
 menuEmail.addEventListener("click", toggleDesktopMenu)
+menuHamIcon.addEventListener("click", toggleMobileMenu)
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle("inactive")
 }
+
+function toggleMobileMenu(){
+    mobileMenu.classList.toggle("inactive")
+}
+
+
+
+
 
 
